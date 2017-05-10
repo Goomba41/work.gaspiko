e0c85788d215e850775fcf3317086c457bf20b3f
Goomba41 <anton.borodawkin@yandex.ru>
2017-05-10 11:13:01 +0300

    История действий

--- app/models.py
+++ app/models.py
@@ -24,6 +24,7 @@ class User(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey("role.id"))
 
     important_news = db.relationship('Important_news', backref = 'user',lazy = 'dynamic')
+    history = db.relationship('History', backref = 'user_parent',lazy = 'dynamic')
 
     def __repr__(self):
         return '<Users %r>' % (self.name)
@@ -48,12 +49,12 @@ class Post(db.Model):
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(15))
+    name = db.Column(db.String(50))
 
     users = db.relationship('User', backref = 'role',lazy = 'dynamic')
 
-    #~ def __repr__(self):
-        #~ return '<Role %r >' % (self.name)
+    def __repr__(self):
+        return '<Role %r >' % (self.name)
 
 class Important_news(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -64,3 +65,33 @@ class Important_news(db.Model):
 
     def __repr__(self):
         return '<Good news everyone! %r >' % (self.text)
+
+class Module(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+
+    tables = db.relationship('Table', backref = 'module_parent',lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<Module! %r >' % (self.name)
+
+class Table(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    module = db.Column(db.Integer, db.ForeignKey("module.id"))
+    name = db.Column(db.String(50))
+    url = db.Column(db.String(15))
+
+    history = db.relationship('History', backref = 'table_parent',lazy = 'dynamic')
+
+    def __repr__(self):
+        return 'Table %r >' % (self.name)
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    cdate = db.Column(db.DateTime, default=time.strftime("%Y-%m-%d %H:%M:%S"))
+    action = db.Column(db.String(50))
+    table = db.Column(db.Integer, db.ForeignKey("table.id"))
+
+    def __repr__(self):
+        return 'Table %r >' % (self.name)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 ﻿#! venv/bin/python
 
 from app import app, db
-from models import User, Department, Role, Post, Important_news
+from models import User, Department, Role, Post, Important_news, Table, History
 from forms import LoginForm, DelUserForm, AddUserForm, EditUserForm, AddRoleForm, DelRoleForm, AddDepartmentForm, DelDepartmentForm, AddPostForm, DelPostForm, DelImportantForm
 from flask import request, make_response, redirect, url_for, render_template, session, flash, g, jsonify, Response
 from functools import wraps
@@ -44,13 +44,19 @@ def get_current_user():
     current_user = User.query.filter(User.id == session['user_id']).first()
     return current_user
 
+#Добавление в историю
+def make_history(str_table, str_action, current_user_id):
+    table_id = Table.query.filter(Table.url == str_table).first()
+    action = History(user_id=current_user_id, action = str_action, table = table_id.id)
+    db.session.add(action)
+    return "success"
+
 #Счетчики
 def get_counters():
     user_count = User.query.count()
     role_count = Role.query.count()
     department_count = Department.query.count()
     post_count = Post.query.count()
-    #~ user_count = User.query.count()
     counters_dict={}
     for name in ['user_count','role_count','department_count','post_count']:
         counters_dict.update({name:eval(name)})
@@ -145,6 +151,7 @@ def admin():
     if form_delete.validate_on_submit():
         important_id = form_delete.del_id.data
         Important_news.query.filter(Important_news.id == important_id).delete()
+        make_history("important", "удаление", current_user.id)
         db.session.commit()
         return redirect(url_for('admin'))
 
@@ -154,6 +161,7 @@ def admin():
 #Быстрое изменение данных записи
 @app.route('/fast_important_edit', methods = ['POST'])
 def fast_important_edit():
+    current_user = get_current_user()
     request_data = request.form
     edit_data= Important_news.query.get(request_data['pk'])
     if request.method  == 'POST':
@@ -164,6 +172,7 @@ def fast_important_edit():
                 edit_data.expired = request_data['value']
             else:
                 edit_data.expired = None
+        make_history("important", "редактирование", current_user.id)
         db.session.commit()
     response = app.response_class(
         response=json.dumps({"Успешно изменено!":edit_data.id}),
@@ -180,6 +189,7 @@ def new_important():
     if request.method  == 'POST':
         data = Important_news(text=request_data['value'], author = current_user.id, expired=datetime.datetime.now()+datetime.timedelta(days=30))
         db.session.add(data)
+        make_history("important", "вставку", current_user.id)
         db.session.commit()
     destination = url_for('admin')
     response = Response(
@@ -209,6 +219,7 @@ def admin_users(page = 1, *args):
     if form_delete.validate_on_submit():
         user_id = form_delete.del_id.data
         User.query.filter(User.id == user_id).delete()
+        make_history("users", "удаление", current_user.id)
         db.session.commit()
         flash(u"Пользователь удален", 'success')
         return redirect(url_for('admin_users', page = page))
@@ -232,6 +243,7 @@ def get_post_javascript_data_password():
 #Удаление нескольких записей
 @app.route('/rows_delete', methods = ['POST'])
 def get_post_javascript_data_id_delete():
+    current_user = get_current_user()
     ids = request.form.getlist('param[]')
     table = request.form.getlist('table')
     if ids:
@@ -239,18 +251,22 @@ def get_post_javascript_data_id_delete():
             users = User.query.filter(User.id.in_(ids)).all()
             for user in users:
                 db.session.delete(user)
+            make_history("users", "удаление", current_user.id)
         if table[0] == 'role':
             roles = Role.query.filter(Role.id.in_(ids)).all()
             for role in roles:
                 db.session.delete(role)
+            make_history("roles", "удаление", current_user.id)
         if table[0] == 'department':
             departments = Department.query.filter(Department.id.in_(ids)).all()
             for department in departments:
                 db.session.delete(department)
+            make_history("departments", "удаление", current_user.id)
         if table[0] == 'post':
             posts = Post.query.filter(Post.id.in_(ids)).all()
             for post in posts:
                 db.session.delete(post)
+            make_history("posts", "удаление", current_user.id)
         db.session.commit()
         return jsonify(ids)
     return jsonify("Не выбраны записи")
@@ -310,6 +326,7 @@ def new_user():
                 photo = hashname )
 
                 db.session.add(user)
+                make_history("users", "вставку", current_user.id)
                 db.session.commit()
 
                 flash(u"Пользователь добавлен", 'success')
@@ -398,6 +415,7 @@ def edit_user():
                 edit_user.role_id = form_user_edit.role_id.data
                 edit_user.photo = hashname
 
+                make_history("users", "редактирование", current_user.id)
                 db.session.commit()
 
                 flash(u"Пользователь изменен", 'success')
@@ -462,6 +480,7 @@ def admin_roles(page = 1, *args):
     if form_delete.validate_on_submit():
         role_id = form_delete.del_id.data
         Role.query.filter(Role.id == role_id).delete()
+        make_history("roles", "удаление", current_user.id)
         db.session.commit()
         flash(u"Роль удалена", 'success')
         return redirect(url_for('admin_roles', page = page))
@@ -471,10 +490,12 @@ def admin_roles(page = 1, *args):
 #Быстрое изменение данных записи
 @app.route('/fast_role_edit', methods = ['POST'])
 def fast_role_edit():
+    current_user = get_current_user()
     request_user = request.form
     edit_role = Role.query.get(request_user['pk'])
     if request.method  == 'POST':
         edit_role.name = request_user['value']
+        make_history("roles", "редактирование", current_user.id)
         db.session.commit()
     response = app.response_class(
         response=json.dumps({"Успешно изменено!":edit_role.id}),
@@ -495,10 +516,9 @@ def new_role():
 
     if form_role_add.validate_on_submit():
         if request.method  == 'POST':
-
             role = Role(name = form_role_add.name.data)
-
             db.session.add(role)
+            make_history("roles", "вставку", current_user.id)
             db.session.commit()
 
             flash(u"Роль добавлена", 'success')
@@ -525,6 +545,7 @@ def admin_departments(page = 1, *args):
     if form_delete.validate_on_submit():
         department_id = form_delete.del_id.data
         Department.query.filter(Department.id == department_id).delete()
+        make_history("departments", "удаление", current_user.id)
         db.session.commit()
         flash(u"Отдел удален", 'success')
         return redirect(url_for('admin_departments', page = page))
@@ -534,10 +555,12 @@ def admin_departments(page = 1, *args):
 #Быстрое изменение данных записи
 @app.route('/fast_department_edit', methods = ['POST'])
 def fast_department_edit():
+    current_user = get_current_user()
     request_user = request.form
     edit_department = Department.query.get(request_user['pk'])
     if request.method  == 'POST':
         edit_department.name = request_user['value']
+        make_history("departments", "редактирование", current_user.id)
         db.session.commit()
     response = app.response_class(
         response=json.dumps({u'Успешно изменено!':edit_department.id}),
@@ -558,10 +581,9 @@ def new_department():
 
     if form_department_add.validate_on_submit():
         if request.method  == 'POST':
-
             department = Department(name = form_department_add.name.data)
-
             db.session.add(department)
+            make_history("departments", "вставку", current_user.id)
             db.session.commit()
 
             flash(u"Отдел добавлен", 'success')
@@ -588,6 +610,7 @@ def admin_posts(page = 1, *args):
     if form_delete.validate_on_submit():
         post_id = form_delete.del_id.data
         Post.query.filter(Post.id == post_id).delete()
+        make_history("posts", "удаление", current_user.id)
         db.session.commit()
         flash(u"Должность удалена", 'success')
         return redirect(url_for('admin_posts', page = page))
@@ -597,10 +620,12 @@ def admin_posts(page = 1, *args):
 #Быстрое изменение данных записи
 @app.route('/fast_post_edit', methods = ['POST'])
 def fast_post_edit():
+    current_user = get_current_user()
     request_post = request.form
     edit_post = Post.query.get(request_post['pk'])
     if request.method  == 'POST':
         edit_post.name = request_post['value']
+        make_history("posts", "редактирование", current_user.id)
         db.session.commit()
     response = app.response_class(
         response=json.dumps({u'Успешно изменено!':edit_post.id}),
@@ -621,12 +646,49 @@ def new_post():
 
     if form_posts_add.validate_on_submit():
         if request.method  == 'POST':
-
             post = Post(name = form_posts_add.name.data)
-
             db.session.add(post)
+            make_history("posts", "вставку", current_user.id)
             db.session.commit()
 
             flash(u"Должность добавлена", 'success')
             return redirect(url_for('admin_posts'))
     return render_template("admin/add_posts.html", form_posts_add = form_posts_add, all_counters = all_counters, current_user=current_user, today=today)
+
+#Страница с историей действий
+@app.route('/admin/history', methods=['GET', 'POST'])
+@app.route('/admin/history/<int:page>', methods=['GET', 'POST'])
+@login_required
+def admin_history(page = 1, *args):
+
+    current_user = get_current_user()
+    all_counters = get_counters()
+
+    actions_all = History.query.filter(History.user_id == current_user.id).order_by(History.id.desc())
+    all_count = actions_all.count()
+    actions_all = actions_all.paginate(page, 100, False)
+
+    today = time.strftime("%Y-%m-%d")
+    pagination = Pagination(page=page, total = all_count, per_page = 100, css_framework='bootstrap3')
+
+    return render_template("admin/list_history.html", actions_all = actions_all, all_counters = all_counters, pagination = pagination,  current_user=current_user, today=today)
+
+#Страница с историей действий всех пользователей
+@app.route('/admin/history/all', methods=['GET', 'POST'])
+@app.route('/admin/history/all/<int:page>', methods=['GET', 'POST'])
+@login_required
+def admin_history_all(page = 1, *args):
+
+    current_user = get_current_user()
+    if current_user.role.id != 1:
+        return forbidden(403)
+
+    all_counters = get_counters()
+
+    actions_all = History.query.order_by(History.id.desc()).paginate(page, 100, False)
+    all_count = History.query.count()
+
+    today = time.strftime("%Y-%m-%d")
+    pagination = Pagination(page=page, total = all_count, per_page = 100, css_framework='bootstrap3')
+
+    return render_template("admin/list_history_all.html", actions_all = actions_all, all_counters = all_counters, pagination = pagination,  current_user=current_user, today=today)
